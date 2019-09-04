fb11cd084c1da2aa8ea8015e98aa171e83302139
Abhishek Kumar Prasad <abhishek.prasad@nathcorp.com>
2019-09-04 13:00:30 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two nos");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
